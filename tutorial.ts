57c583a56e6518bea9784c78aacfb3ff9a0f9846
Esther Okiki <estherokiki15@gmail.com>
2025-02-28 15:44:52 +0100

    initialise git repo

---
+++ tutorial.ts
@@ -0,0 +1,7 @@
+let id: number = 5;
+let company: string = "Acme Corp";
+let isPublished: boolean = true;
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let x: any = "Esther";
+let xArr: any[] = ["Esther", 0, true];
