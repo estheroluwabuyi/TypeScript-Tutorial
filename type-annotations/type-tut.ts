b611590768e2370020741600f526a77257d527a6
Esther Okiki <estherokiki15@gmail.com>
2025-02-28 16:07:22 +0100

    create type-tut.ts in type-annotations folder

--- type-annotations/type-tut.ts
+++ type-annotations/type-tut.ts
@@ -1,7 +1,13 @@
+//Variable Types
 let id: number = 5;
 let company: string = "Acme Corp";
 let isPublished: boolean = true;
 
+//Array Types
 let ids: number[] = [1, 2, 3, 4, 5];
+
+//Any Types
 let x: any = "Esther";
 let xArr: any[] = ["Esther", 0, true];
+
+//Function Types
